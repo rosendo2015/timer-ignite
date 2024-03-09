875ad7169dc8469b335b0f40a99ba9c4df871e47
rosendo <rosendc30@gmail.com>
2024-03-09 10:45:18 -0300

    correção de bugs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,12 +20,7 @@ const newCycleFormValidationSchema = zod.object({
     .min(5, 'Valor mínimo 5 minutos')
     .max(60, 'Valor máximo 60 minutos'),
 })
-/*
-  interface NewCycleFormData {
-  task: string
-  minutesAmount: number
-}
-*/
+
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 interface Cycle {
@@ -45,31 +40,38 @@ export function Home() {
     defaultValues: {
       task: '',
       minutesAmount: 0,
-      startDate: new Date(),
     },
   })
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
   useEffect(() => {
+    let interval: number
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
     }
+    return () => {
+      clearInterval(interval)
+    }
   }, [activeCycle])
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
+
     const newCycle: Cycle = {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     }
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
@@ -82,6 +84,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
